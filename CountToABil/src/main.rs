ace8bc92280f638475f022739703f02f3bda2415
malkiium <elihalldelafosse@gmail.com>
2025-10-10 11:57:45 +0200

    Fix output frequency in main loop to print every 100 million iterations

--- CountToABil/src/main.rs
+++ CountToABil/src/main.rs
@@ -2,7 +2,7 @@ fn main() {
     let mut x = 0;
     while x < 1000000000 {
         x+=1;
-        if x%10000000 == 0 {
+        if x%100000000 == 0 {
             println!("{}", x);
         }
     }
