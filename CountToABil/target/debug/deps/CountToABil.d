5ac298b03ed8dad85f74966d8b34b25982fb049f
malkiium <elihalldelafosse@gmail.com>
2025-10-23 16:49:25 +0200

    Add incremental build artifacts and linker error diagnostics
    - Created new incremental build artifacts in the target directory.
    - Added lock files for incremental compilation.
    - Generated cargo lock file for release.
    - Included timestamp file for build invocation.
    - Documented linker error indicating that `link.exe` was not found, with suggestions for resolution.
    - Added object files for the release build, including dependencies and generated code.
    - Updated dependency tracking for the main source file.

---
+++ CountToABil/target/debug/deps/CountToABil.d
@@ -0,0 +1,5 @@
+C:\Users\eliha\vsc\Rust\CountToABil\target\debug\deps\CountToABil.d: src\main.rs
+
+C:\Users\eliha\vsc\Rust\CountToABil\target\debug\deps\CountToABil.exe: src\main.rs
+
+src\main.rs:
---
+++ CountToABil/target/release/deps/CountToABil.d
@@ -0,0 +1,5 @@
+C:\Users\eliha\vsc\Rust\CountToABil\target\release\deps\CountToABil.d: src\main.rs
+
+C:\Users\eliha\vsc\Rust\CountToABil\target\release\deps\CountToABil.exe: src\main.rs
+
+src\main.rs:
